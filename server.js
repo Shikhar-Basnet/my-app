4801e9e5b2bd8fe40e7377d80c6944f0eedbf59c
Shikhar Basnet <shikharbasnet123@gmail.com>
2026-09-16 21:16:58 +0545

    Initial commit

---
+++ server.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/api/hello', (req, res) => {
+  res.json({ message: "Hello from my GCP VM" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
